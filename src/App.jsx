ff721f643eda9965167815bf784a45a8842d9771
Ramirez <daniel.ramirez@siemens.com>
2025-07-02 13:23:00 -0600

    primer commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import {Navbar} from './Navbar.jsx';
+import {Footer} from './Footer.jsx';
+
+function App() {
+
+  const unaVar = "Unavar";
+
+  return (
+    <div>
+      <Navbar />
+      Hola desde React
+      <Footer />
+    </div>
+  );
+}
+
+export default App
---
+++ src/Navbar.jsx
@@ -0,0 +1,14 @@
+export const Navbar = () => {
+    return (
+       <div>
+            <h2>Tienda de TCG</h2>
+            <nav>
+                <ul>
+                    <li>Home</li>
+                    <li>Yugioh</li>
+                    <li>Pokemon</li>
+                </ul>
+            </nav>
+       </div>
+    );
+} 
