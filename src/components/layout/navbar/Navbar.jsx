b3dd48b9ca0ded7385d02749b465a03e90daae94
Ramirez <daniel.ramirez@siemens.com>
2025-08-30 17:18:01 -0600

    Correcciones

--- src/components/layout/navbar/Navbar.jsx
+++ src/components/layout/navbar/Navbar.jsx
@@ -1,5 +1,5 @@
 import "./Navbar.css";
-import { CartWidget } from "../../cartwidget/cartWidget";  
+import { CartWidget } from "../../cartwidget/CartWidget.jsx"; 
 import { Link } from "react-router";
 
 
