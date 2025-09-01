6077939d7d319e97389b94a95ed05aca93c9ad17
Ramirez <daniel.ramirez@siemens.com>
2025-08-31 23:09:29 -0600

    Mensaje de toastify al completar orden

--- src/components/pages/checkout/Checkout.jsx
+++ src/components/pages/checkout/Checkout.jsx
@@ -4,6 +4,8 @@ import {db} from "../../../firebaseConfig"
 import {useContext} from 'react';
 import { CartContext } from '../../../context/CartContext.jsx';
 import {addDoc, collection, doc, updateDoc} from "firebase/firestore"
+import { ToastContainer, toast } from 'react-toastify';
+import "./Checkout.css"
 
 export const Checkout = () => {
 
@@ -16,6 +18,8 @@ export const Checkout = () => {
     const {cart, getTotalAmount, resetCart} = useContext(CartContext);
 
     const [orderId, setOrderId] = useState(null);
+
+    const pruebaToast = () => toast("Orden generada con exito");
 
     const handleSubmit = (evento)=>{
         evento.preventDefault();
@@ -41,19 +45,25 @@ export const Checkout = () => {
             let documento = doc(productsCollection, producto.id);
             updateDoc(documento, {stock:producto.stock - producto.cantidad});
         })
+
+        pruebaToast();
     }
     
 
     return (
-        <div>
-            {orderId ? <strong>Gracias por tu compra! Tu numero de orden es : {orderId}</strong> :
+        <div className='order-main-container'>
+            {orderId ? <strong>Gracias por tu compra! Tu numero de orden es : {orderId}</strong> : 
             <form onSubmit={handleSubmit}>
-                <input name="nombre" type="text" placeholder="Nombre" onChange={(evento)=>setUser({...user, nombre:evento.target.value})}/>
-                <input name="telefono" type="text" placeholder="telefono" onChange={(evento)=>setUser({...user, telefono:evento.target.value})}/>
-                <input name="email" type="text" placeholder="email" onChange={(evento)=>setUser({...user, email:evento.target.value})}/>
-                <button>Comprar</button>
+                <div className="form-container"> 
+                    <h2>Datos del Cliente</h2>
+                    <input name="nombre" type="text" placeholder="Nombre" onChange={(evento)=>setUser({...user, nombre:evento.target.value})}/>
+                    <input name="telefono" type="text" placeholder="telefono" onChange={(evento)=>setUser({...user, telefono:evento.target.value})}/>
+                    <input name="email" type="text" placeholder="email" onChange={(evento)=>setUser({...user, email:evento.target.value})}/>
+                    <button className="product-btn">Finalizar compra</button>                    
+                </div>
             </form>
             }
+            <ToastContainer position="top-right" autoClose={5000} hideProgressBar={false} newestOnTop={false} closeOnClick rtl={false} pauseOnFocusLoss draggable pauseOnHover theme="light"/>
         </div>
     )
 }
